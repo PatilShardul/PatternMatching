07465f37565870fcaeda0f324aedea4ef6eaaeb7
Shardul Patil <patilshardulsatish@yahoo.com>
2020-12-13 15:12:13 +0530

    Refactor: Added code to check email is in abc.xyz@bl.co.in

--- userRegisterPattern.sh
+++ userRegisterPattern.sh
@@ -1,10 +1,17 @@
+#!/usr/bin/bash -x
+shopt -s  extglob
 
 echo "Welcome To User Registeration Check"
 
 read -p "Enter First Name : " firstName
 read -p "Enter Last Name : " lastName
+read -p "Enter User Email : " email
+
 firstNamePattern=^[A-Z][a-zA-Z]{2}
+
 lastNamePattern=^[A-Z][a-zA-Z]{2}
+
+emailPattern=^[a-z]+([.][a-z]+)?@[a-z]+.[a-z]+([.][a-z]+)?$
 
 if [[ $firstName =~ $firstNamePattern ]]
 then
@@ -18,5 +25,12 @@ then
 	echo "$lastName"
 else
 	echo "Invalid Last Name"
+fi
+
+if [[ $email =~ $emailPattern ]]
+then
+	echo "$email"
+else
+	echo "Invalid Email"
 fi
 
