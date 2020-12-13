4c63a1fb358f46b8b12944830a32eef8b2742dd1
Shardul Patil <patilshardulsatish@yahoo.com>
2020-12-13 14:31:09 +0530

    Refactor : Add the code to check if First name starts with capital letter and is greater than 3 characters

--- userRegisterPattern.sh
+++ userRegisterPattern.sh
@@ -1,4 +1,13 @@
 
 echo "Welcome To User Registeration Check"
 
+read -p "Enter First Name" firstName
 
+firstNamePattern=^[A-Z][a-zA-Z]{2}
+
+if [[ $firstName =~ $firstNamePattern ]]
+then
+	echo "$firstName"
+else 
+	echo "Invalid First Name"
+fi
