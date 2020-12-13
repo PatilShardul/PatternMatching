4142b4f8328ebade93da21feaef79f9c66ec763b
Shardul Patil <patilshardulsatish@yahoo.com>
2020-12-13 14:38:19 +0530

    Refactor  : Added code to check last name patter Starting with Capital Lette and has minimum 3 character

--- userRegisterPattern.sh
+++ userRegisterPattern.sh
@@ -1,13 +1,22 @@
 
 echo "Welcome To User Registeration Check"
 
-read -p "Enter First Name" firstName
-
+read -p "Enter First Name : " firstName
+read -p "Enter Last Name : " lastName
 firstNamePattern=^[A-Z][a-zA-Z]{2}
+lastNamePattern=^[A-Z][a-zA-Z]{2}
 
 if [[ $firstName =~ $firstNamePattern ]]
 then
 	echo "$firstName"
-else 
+else
 	echo "Invalid First Name"
 fi
+
+if [[ $lastName =~ $lastNamePattern ]]
+then
+	echo "$lastName"
+else
+	echo "Invalid Last Name"
+fi
+
