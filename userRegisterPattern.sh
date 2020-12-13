ee61ba6537b3ff93076899a7dc9d24016fe0b2e2
Shardul Patil <patilshardulsatish@yahoo.com>
2020-12-13 15:24:28 +0530

    Refactor : Added Code to check mobile number pattern 'XX XXXXXXXXXX'

--- userRegisterPattern.sh
+++ userRegisterPattern.sh
@@ -6,12 +6,14 @@ echo "Welcome To User Registeration Check"
 read -p "Enter First Name : " firstName
 read -p "Enter Last Name : " lastName
 read -p "Enter User Email : " email
-
+read -p "Enter Mobile Number In '(country code)xx (number)xxxxxxxxxx' Format : " mobileNumber
 firstNamePattern=^[A-Z][a-zA-Z]{2}
 
 lastNamePattern=^[A-Z][a-zA-Z]{2}
 
 emailPattern=^[a-z]+([.][a-z]+)?@[a-z]+.[a-z]+([.][a-z]+)?$
+
+mobileNumberPattern=^[0-9][1-9]"\s"[0-9]{10}
 
 if [[ $firstName =~ $firstNamePattern ]]
 then
@@ -32,5 +34,12 @@ then
 	echo "$email"
 else
 	echo "Invalid Email"
+fi
+
+if [[ $mobileNumber =~ $mobileNumberPattern ]]
+then
+	echo "+$mobileNumber"
+else
+	echo "Invalid Mobile Number"
 fi
 
